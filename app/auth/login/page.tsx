91aa1bdb12e2200bfeb9ecab9a7ea28ddf6d5f7f
nobody <somebodyyyyy12321@gmail.com>
2025-12-29 20:36:27 +0800

    test

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,7 +3,7 @@
 import React, { Suspense, useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import SearchParamsProvider, { useSearchParamsContext } from "../../components/SearchParamsProvider";
+import SearchParamsProvider, { useSearchParamsContext } from "@/components/SearchParamsProvider";
 
 function LoginInner() {
   const [email, setEmail] = useState("");
