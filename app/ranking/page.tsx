39ef05e964e16e4f29c48343899d6e905bcc44df
nobody <somebodyyyyy12321@gmail.com>
2025-12-31 19:15:23 +0800

    字往下移

--- app/ranking/page.tsx
+++ app/ranking/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function RankingPage() {
   return (
-    <main className="max-w-5xl mx-auto py-24 px-6 flex items-center justify-center">
+    <main className="max-w-5xl mx-auto pt-40 px-6 flex items-center justify-center">
       <div className="text-center text-2xl">敬請期待</div>
     </main>
   );
