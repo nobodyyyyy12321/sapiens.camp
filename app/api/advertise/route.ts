631af01f1a50fa59448a3efc0f3ad9b7cb53b761
nobody <somebodyyyyy12321@gmail.com>
2026-01-14 09:40:03 +0800

    n

--- app/api/advertise/route.ts
+++ app/api/advertise/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
-import Resend from "resend";
+import { Resend } from "resend";
 
 type Body = {
   contact: string;
